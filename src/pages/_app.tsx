ba4fd6b8673f23226e651d8e0e3fe0ce8d7e8e43
masayuki0228 <hirosawa0228@icloud.com>
2022-04-21 23:07:55 +0900

    💄 商品一覧ページのスタイリング（仮）

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Header } from "src/components/header";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Header />
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { MicroCMSListResponse } from "microcms-js-sdk";
 import type { GetStaticProps, NextPage } from "next";
+import Image from "next/image";
 import Link from "next/link";
 import { client } from "src/libs/client";
 
@@ -13,14 +14,22 @@ type Props = MicroCMSListResponse<Product>;
 
 const Home: NextPage<Props> = (props) => {
   return (
-    <div>
-      <p>{`ラインナップ: ${props.totalCount}種類`}</p>
-      <ul>
+    <div className="container mx-auto p-4">
+      <ul className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         {props.contents.map((content) => {
           return (
-            <li key={content.id}>
+            <li key={content.id} className="group relative">
               <Link href={`/products/${content.id}`}>
-              <a>{content.name}</a>
+                <a>
+                  <Image
+                    width={600}
+                    height={400}
+                    src="https://images.unsplash.com/photo-1599637777203-34ac21aa929b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2888&q=80"
+                    alt="This is coffee beeeeeeeean."
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                  {content.name}
+                </a>
               </Link>
               <div>¥{content.price}から</div>
             </li>
