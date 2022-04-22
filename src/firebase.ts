827ed3dbf73b02fdc41f5a6c4055f3ef7c591b88
masayuki0228 <hirosawa0228@icloud.com>
2022-04-22 21:34:58 +0900

    ➕ add firebase/firestore

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAByHYC59HTD8HAZuDWqV9K-w7KTSnvrNY",
+  authDomain: "coffee-shop-13638.firebaseapp.com",
+  projectId: "coffee-shop-13638",
+  storageBucket: "coffee-shop-13638.appspot.com",
+  messagingSenderId: "535191629668",
+  appId: "1:535191629668:web:739957a16f27a0627a9471",
+};
+
+const app = initializeApp(firebaseConfig);
