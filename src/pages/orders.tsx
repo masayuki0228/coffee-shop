4614c1cbfa51c79c859cbc6abf80debe61cfa33c
masayuki0228 <hirosawa0228@icloud.com>
2022-04-30 12:01:11 +0900

    🔒 ♻️ ordersTableをコンポーネント化 ＆ ordersをTS化 ＆ リファクタリング

--- src/pages/orders.tsx
+++ src/pages/orders.tsx
@@ -3,62 +3,25 @@ import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import { db } from "src/firebase";
 import dayjs from "dayjs";
 import Head from "next/head";
+import { OrdersTable } from "src/components/ordersTable";
+import { Order } from "src/types/order";
 
-export type Order = {
-  id: string;
-  productName: string;
-  userName: string;
-  address: string;
-  price: {
-    totalPrice: number;
-  };
-  date: string;
+type Props = {
+  orders: Order[];
 };
 
-const Orders: NextPage<any> = (props) => {
-  const { orders } = props;
-  if (!orders) {
-    return <div>ローディング中</div>;
-  }
-  if (orders && orders.length === 0) {
-    return <div>データは空です</div>;
-  }
+const Orders: NextPage<Props> = ({ orders }) => {
   return (
     <div>
       <Head>
         <title>【Coffee Shop】 注文一覧</title>
       </Head>
-      <table className="m-6 overflow-x-auto whitespace-nowrap p-4">
-        <thead className="flex-shrink-0">
-          <tr>
-            <th className="border px-4 py-2">注文Id</th>
-            <th className="border px-4 py-2">お名前</th>
-            <th className="border px-4 py-2">お届け先</th>
-            <th className="border px-4 py-2">商品名</th>
-            <th className="border px-4 py-2">価格</th>
-            <th className="border px-4 py-2">購入日時</th>
-          </tr>
-        </thead>
-        {orders.map((order: Order) => {
-          return (
-            <tbody key={order.id} className="flex-shrink-0">
-              <tr>
-                <td className="border px-2 py-1">{order.id}</td>
-                <td className="border px-2 py-1">{order.userName}</td>
-                <td className="border px-2 py-1">{order.address}</td>
-                <td className="border px-2 py-1">{order.productName}</td>
-                <td className="border px-2 py-1">¥{order.price.totalPrice}</td>
-                <td className="border px-2 py-1">{order.date}</td>
-              </tr>
-            </tbody>
-          );
-        })}
-      </table>
+      <OrdersTable orders={orders} />
     </div>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
   const orders: Order[] = [];
   const q = query(collection(db, "orders"), orderBy("timestamp", "desc"));
   const querySnapshot = await getDocs(q);
