4299a0013f659e7f1c7f895e8ec397a64e248a70
masayuki0228 <hirosawa0228@icloud.com>
2022-05-04 22:13:30 +0900

    🐛

--- src/components/ordersTable.tsx
+++ src/components/ordersTable.tsx
@@ -36,16 +36,16 @@ export const OrdersTable: VFC<Props> = ({ orderList, setOrderList }) => {
       await updateDoc(washingtonRef, {
         sent: true,
       });
-      const orders: Order[] = [];
-      orderList.map((order) => {
-        const data = order;
-        if (undispatchedList.includes(order.id)) {
-          order.sent = !order.sent;
-        }
-        orders.push(data);
-      });
-      setOrderList(orders);
     });
+    const orders: Order[] = [];
+    orderList.map((order) => {
+      const data = order;
+      if (undispatchedList.includes(order.id)) {
+        data.sent = true;
+      }
+      orders.push(data);
+    });
+    setOrderList(orders);
   };
 
   if (!orderList) {
