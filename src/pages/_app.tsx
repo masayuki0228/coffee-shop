8a43936ca7db611e0a864e85ad188b5901576f0f
masayuki0228 <hirosawa0228@icloud.com>
2022-05-05 21:23:02 +0900

    ✨ firebaseauthを使用して、ログイン機能を追加しました。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "src/firebase";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [admin, setAdmin] = useState<{} | null>("");
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setAdmin(currentUser);
+    });
+  }, []);
+
+  return <Component {...pageProps} admin={admin} setAdmin={setAdmin} />;
 }
 
 export default MyApp;
